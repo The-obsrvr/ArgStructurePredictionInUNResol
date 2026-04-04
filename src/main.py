b92f0bf7ea56e117ef1ea38ed3501658e6988e77
The-obsrvr <sddhrth.bhargava@gmail.com>
2026-04-04 13:41:57 +0200

    step1 redo

--- src/main.py
+++ src/main.py
@@ -80,8 +80,8 @@ def main():
     outputs3: 5
     output4: 6
     """
-    input_folder = "outputs2"
-    output_folder = "outputs22"
+    input_folder = "outputs"
+    output_folder = "outputs11"
     # create output folder
     os.makedirs(output_folder, exist_ok=True)
 
