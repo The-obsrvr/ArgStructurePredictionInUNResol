129ecedf0c51824bc128b9e440b2a483e4042926
The-obsrvr <sddhrth.bhargava@gmail.com>
2026-04-10 11:35:15 +0200

    update

--- src/download_data.py
+++ src/download_data.py
@@ -5,5 +5,5 @@ snapshot_download(
     repo_id="ZurichNLP/ArgMining-2026-UZH-Shared-Task",
     repo_type="dataset",
     token="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",
-    local_dir="./Data",
+    local_dir="../Data",
     )
